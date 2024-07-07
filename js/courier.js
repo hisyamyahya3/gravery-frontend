52db81f95015f304943ac7a1bd8b23bc744173ed
kito <surya.aw26@gmail.com>
2024-07-07 21:01:52 +0700

    fetch content ongoing and add dialog confirm for finish pickup

--- js/courier.js
+++ js/courier.js
@@ -37,29 +37,55 @@ showListOrder = () => {
 }
 
 showOngoing = () => {
+    let courierId = sessionStorage.getItem("courierId");
+
     $.ajax({
-        url: "https://gravery-api.vercel.app/api/list-order",
-        method: "GET",
+        url: "https://gravery-api.vercel.app/api/ongoing-order",
+        method: "POST",
+        data: { courier_id: courierId },
         success: function (result) {
-            console.log(result);
+            const data = result.data
 
-            let temp = ''
+            if (data == null) {
+                return $('.ongoing-content-wrapper').html(`
+                    <div class="card">
+                        <div class="card-content card-content-padding">
+                            <h2>${result.message}</h2>
+                        </div>
+                    </div>
+                `)
+            }
 
-            result.data.forEach((d) => {
-                if (d.is_picked_up && !d.is_finished) {
-                    temp += `<div class="card">
+            let orderTemplate = ''
+            let detailOrderTemplate = ''
+
+            data.forEach((d) => {
+                orderTemplate += `<div class="card">
+                            <div class="card-content card-content-padding">
+                                <p>Nama Pelanggan: ${d.customer_name}</p>
+                                <p>Alamat Pelanggan: ${d.customer_address}</p>
+                                <p>Total: ${rupiahFormatter(d.total)}</p>
+                                <p>Tanggal Order: ${d.parsedOrderDate}</p>
+                                <div class="left">
+                                    <button class="button button-small button-tonal" onclick="finishPickup(${d.id})">Selesaikan Pesanan</button>
+                                </div>
+                            </div>
+                        </div>`
+
+                d.orderItems.forEach((orderItems) => {
+                    detailOrderTemplate += `<div class="card">
                                 <div class="card-content card-content-padding">
-                                    <h2 class="col" style="font-weight: bold;">${d.customer.name}</h2>
-                                    <p>Total: ${rupiahFormatter(d.total)}</p>
-                                    <div class="left">
-                                        <button class="button button-small button-tonal" onclick="detailOngoing(${d.customer_id})">Selesaikan Pesanan</button>
-                                    </div>
+                                    <h2 class="col" style="font-weight: bold;">Jenis Sampah: ${(orderItems.garbage_id == 1) ? 'Organic' : 'Anorganic'}</h2>
+                                    <p>Berat (Dalam Kg): ${orderItems.weight}</p>
+                                    <p>Harga: ${rupiahFormatter(orderItems.price)}</p>
                                 </div>
                             </div>`
-                }
+                })
             })
 
-            $('#ongoing-list-order').html(temp);
+            $('.ongoing-content').removeClass('display-none')
+            $('#ongoing-list-order').html(orderTemplate);
+            $('#pickup-detail-orderitems').html(detailOrderTemplate)
         },
         error: function () {
             app.dialog.alert("Tidak Terhubung dengan Server!", "Error");
@@ -94,21 +120,24 @@ detailOngoing = (id) => {
     app.views.main.router.navigate(`/detail-ongoing/${id}`);
 }
 
-finishPickup = (id) => {
+finishPickup = (orderID) => {
     let courierId = sessionStorage.getItem("courierId");
-    $.ajax({
-        url: `https://gravery-api.vercel.app/api/finish/pickup-order/${id}`,
-        method: "POST",
-        data: {
-            courier_id: courierId,
-        },
-        success: function (result) {
-            let message = result.message;
-            console.log(result);
-            app.dialog.alert(message, "Info")
-        },
-        error: function () {
-            app.dialog.alert("Tidak Terhubung dengan Server!", "Error");
-        }
+
+    app.dialog.confirm('Apa kamu yakin ingin menyelesaikan pesanan ini?', 'Info', () => {
+        $.ajax({
+            url: `https://gravery-api.vercel.app/api/finish/pickup-order/${orderID}`,
+            method: "POST",
+            data: {
+                courier_id: courierId,
+            },
+            success: function (result) {
+                let message = result.message;
+                app.dialog.alert(message, "Info")
+                app.views.main.router.refreshPage()
+            },
+            error: function () {
+                app.dialog.alert("Tidak Terhubung dengan Server!", "Error");
+            }
+        })
     })
 }
